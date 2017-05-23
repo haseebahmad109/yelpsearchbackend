b0ffb9b0b3db43ff30bd1ba6d029e986692c4970
Haseeb Ahmad <haseebahmad109@gmail.com>
2017-05-23 16:15:15 +0500

    remove unnessassary redirect

--- yelpsearch/urls.py
+++ yelpsearch/urls.py
@@ -16,11 +16,8 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
-from django.views.generic.base import RedirectView
-
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', RedirectView.as_view(url='/search', permanent=False)),
 
     url(r'^', include('apps.searchbusiness.urls'))
 
